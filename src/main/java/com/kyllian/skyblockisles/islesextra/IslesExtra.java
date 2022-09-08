1273bb60398dc563f55938f2dab1907b58e77a05
Kyllian <kylleke@telenet.be>
2022-09-08 17:54:12 +0200

    gitlab push

--- src/main/java/com/kyllian/skyblockisles/islesextra/IslesExtra.java
+++ src/main/java/com/kyllian/skyblockisles/islesextra/IslesExtra.java
@@ -44,9 +44,7 @@ public class IslesExtra implements ModInitializer {
                 }
             }
         }));
-
         registerAttributes();
-
     }
 
     private static void registerAttributes() {
