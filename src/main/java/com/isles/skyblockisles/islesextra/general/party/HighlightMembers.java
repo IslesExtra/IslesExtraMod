ab8095fb76b497935c7966a5d0287ec5b00f4002
Kyllian <42489040+KyllianGamer@users.noreply.github.com>
2024-02-13 21:58:39 +0100

    glow players
    add glowing players

--- src/main/java/com/isles/skyblockisles/islesextra/general/party/HighlightMembers.java
+++ src/main/java/com/isles/skyblockisles/islesextra/general/party/HighlightMembers.java
@@ -4,14 +4,21 @@ import com.isles.skyblockisles.islesextra.utils.ClientUtils;
 import com.isles.skyblockisles.islesextra.utils.PartyUtils;
 import net.minecraft.entity.player.PlayerEntity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HighlightMembers {
+
+    private static final List<PlayerEntity> glowingPlayers = new ArrayList<>(4);
+
+    public static List<PlayerEntity> getGlowingPlayers() {
+        return glowingPlayers;
+    }
 
     public static void init() {
         if(PartyUtils.getMembers().isEmpty() || ClientUtils.getClient().getNetworkHandler() == null) return;
-        for (PlayerEntity playerEntity : PartyUtils.partyMemberEntities) {
-            //TODO: ADD GLOWING EFFECT
-        }
-
+        //TODO: ADD GLOWING EFFECT
+        glowingPlayers.addAll(PartyUtils.partyMemberEntities);
     }
 
 }
