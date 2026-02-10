74d8ad89d2f0f3d313880239f9989c41fd38dbb7
Ethan Chan <ethanchan7981@gmail.com>
2026-02-09 23:01:56 -0500

    Update islesextra.mixins.json and IslesConfig.java

--- src/main/java/net/skyblockisles/islesextra/config/IslesConfig.java
+++ src/main/java/net/skyblockisles/islesextra/config/IslesConfig.java
@@ -7,8 +7,11 @@ import dev.isxander.yacl3.config.v2.api.serializer.GsonConfigSerializerBuilder;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.util.Identifier;
 import net.skyblockisles.islesextra.IslesExtra;
+import net.skyblockisles.islesextra.constants.IslesShortcutWidget;
 
 import java.awt.*;
+import java.util.List;
+
 
 public class IslesConfig {
     public static ConfigClassHandler<IslesConfig> HANDLER = ConfigClassHandler.createBuilder(IslesConfig.class)
@@ -37,6 +40,12 @@ public class IslesConfig {
 
     @SerialEntry
     public Double partyLowHealthThreshold = 0.2;
+
+    @SerialEntry
+    public boolean lowInventoryEnable = true;
+
+    @SerialEntry
+    public List<IslesShortcutWidget.ShortcutData> inventoryShortcuts = ModmenuIntegration.shortcuts;
 
     @SerialEntry
     public boolean enableWorldBossTimer = true;
